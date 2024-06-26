5a0af301e72b5dfaab060a0f3c11defbc170d0a4
Thiago Batista <thiago.novaes.batista@gmail.com>
2024-06-25 22:45:46 -0300

    Initial commit of Django_AZ_projeto1

---
+++ medicSearch/models/__init__.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+ROLE_CHOICE = (
+    (1, 'Admin'),
+    (2, 'Medico'),
+    (3, 'Patiente')
+)
+
+from .Rating import Rating
+from .DayWeek import DayWeek
+from .State import State
+from .City import City
+from .Neighborhood import Neighborhood
+from .Adress import Adress
+from .Speciality import Speciality
+from .Profile import Profile
